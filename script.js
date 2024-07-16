5f6565a9b9ccaf980d7e1e91db922c864fc23e11
Janice Lok <160542635+janice-lok@users.noreply.github.com>
2024-07-15 21:39:49 -0400

    Add logic to perform calculator operation and update display when  operation is completed

--- script.js
+++ script.js
@@ -1,55 +1,102 @@
-// create user interface for the calculator: 
-let operators = ["/", "*", "-", "+", "="];
-
 const leftDiv = document.querySelector("#left-side");
 const rightDiv = document.querySelector("#right-side");
+const calcDisplay = document.querySelector("#display");
+let displayValue = "", firstNumber = "", secondNumber = "", operator; 
+
+function populateDisplay(val) {
+    calcDisplay.textContent = `${val}`;
+}
 
 function createButton(text) {
     const btn = document.createElement("button"); 
     btn.setAttribute("style", "background-color: rgb(63, 105, 179)");
-    btn.style.border = "2px solid rgb(38, 56, 86)";
-    btn.style.borderRadius = "0.75vw";
+    btn.style.borderRadius = "1vw";
     btn.textContent = `${text}`;
     return btn;
 }
+
 const clearBtn = createButton("CLEAR");
-const zeroBtn = createButton("0");
-
-zeroBtn.setAttribute("style", "width: 180px");
-zeroBtn.style.backgroundColor = "rgb(63, 105, 179)";
-zeroBtn.style.borderRadius = "1vw";
-leftDiv.appendChild(zeroBtn);
-
-for (let i=1; i<10; i++) {
-    const numberBtn = createButton(i);
-    leftDiv.appendChild(numberBtn);
-}
-
 clearBtn.setAttribute("style", "width: 180px");
 clearBtn.style.backgroundColor = "rgb(63, 105, 179)";
 clearBtn.style.borderRadius = "1vw";
 leftDiv.appendChild(clearBtn);
 
+
+for (let i=9; i>-1; --i) {
+    const numberBtn = createButton(i);
+    leftDiv.appendChild(numberBtn);
+}
+
+let operators = ["/", "*", "-", "+", "="];
 operators.map((operator) => {
     const opBtn = createButton(operator);
-    return rightDiv.appendChild(opBtn);
+    rightDiv.appendChild(opBtn);
 });
 
+function storeVal(displayVal) {
+    let opIndex;
+    let arr = displayVal.split('');
+    console.log(arr);
+    arr.forEach(char => {
+        if (operators.includes(char)) {
+            opIndex = arr.indexOf(char);
+            operator = char; 
+        } else {
+            if (opIndex == undefined) {
+                firstNumber += char;
+                console.log("firstNumber:", firstNumber);
+            } else {
+                secondNumber += char;
+                console.log("secondNumber:", secondNumber);
+            }
+        }
+    })
+}
 
-// logic to make the calculator work: 
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("mouseover", () => {
+        button.style.backgroundColor = "rgb(217, 228, 248)";
+        button.style.color = "rgb(63, 105, 179)";
+    });
+    button.addEventListener("mouseout", () => {
+        button.style.backgroundColor = "rgb(63, 105, 179)";
+        button.style.color = "rgb(217, 228, 248)";
+    })
+    button.addEventListener("click", () => {
+        if (button.textContent == "CLEAR") {
+            displayValue = "";
+        } else if (button.textContent == "=") {
+            storeVal(displayValue);
+            displayValue = operate(operator, firstNumber, secondNumber);
+        } else {
+            displayValue += button.textContent;
+        }
+        populateDisplay(displayValue);
+    })
+})
+
 function add(firstNum, secondNum) {
     return firstNum + secondNum;
 }
-
 function subtract(firstNum, secondNum) {
     return firstNum - secondNum;
 }
-
 function multiply(firstNum, secondNum) {
     return firstNum * secondNum;
 }
-
 function divide(firstNum, secondNum) {
     return firstNum / secondNum;
 }
 
+function operate(operator, firstNum, secondNum) {
+    if (operator == "+") {
+        return add(parseInt(firstNum), parseInt(secondNum));
+    } else if (operator == "-") {
+        return subtract(firstNum, secondNum);
+    } else if (operator == "*") {
+        return multiply(firstNum, secondNum);
+    } else {
+        return divide(firstNum, secondNum);
+    }
+}
